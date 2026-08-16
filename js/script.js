613a80728ba12afc9163a7e5ff692459d00823d8
Rachel31-star <successsunday311@gmail.com>
2026-08-16 20:29:10 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+const menuButton = document.getElementById("menuButton");
+const closeButton = document.getElementById("closeButton");
+const sidebar = document.getElementById("sidebar");
 
+menuButton.addEventListener("click", function () {
+    sidebar.classList.add("open");
+});
+
+closeButton.addEventListener("click", function () {
+    sidebar.classList.remove("open");
+});
